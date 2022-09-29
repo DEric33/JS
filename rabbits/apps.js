7e42f5973e7e38c924a22739c246c5a9c5728767
DEric33 <eric.darmusie@darmuweb.com>
2022-09-29 23:16:28 +0200

    Rabbits: Some bugs resolved

--- rabbits/apps.js
+++ rabbits/apps.js
@@ -15,7 +15,7 @@ var prixCaisse; // tarif caisse entre 5 et 8 euros
 var prixCarottes; // tarif carottes entre 2 et 4 euros le kilo
 var prixLitre; // tarif entre .5 et 1.5
 //var coeffRep = 4; // multiplicateur de reproduction
-var periode = 5; // nb de mois d'exploitation : 13 = 1 an , 25 = 2 ans ...
+var periode = 4; // nb de mois d'exploitation : 13 = 1 an , 25 = 2 ans ...
 var portee = 0; // nb de petits
 var accoup = 0; // nb accouplements
 var pf = 0; // nb petites femelles
@@ -39,7 +39,7 @@ function getRandom(min, max) {
     repartition => 2/3 petits 1/3 adultes // 1/2 M/F
     => entree et sortie : aMales pMales aFemelles pFemelles 
  */
-function verifCarottes(aMales,pMales,aFemelles,pFemelles,mCages,fCages){
+function verifCarottes(aMales,pMales,aFemelles,pFemelles){
   var adultes = aMales + aFemelles;
   var petits =  pMales + pFemelles;
   var simule = (adultes * consoCarotteAdulte) + (petits * consoCarottePetit);
@@ -55,7 +55,11 @@ for (month = 1; month < periode; month++) {
 
   // nb d'adultes
   console.log("Males : " + aMales);
+  console.log("Cage(s) Males : "+ mCages);
+  console.log("Males par cage : "+ Math.trunc(aMales/mCages));
   console.log("Femelles : " + aFemelles);
+  console.log("Cage(s) Femelles : "+fCages);
+  console.log("Femelles par cage : "+ Math.trunc(aFemelles/fCages));
 
   // avant accouplements, calcul des consos du cheptel actuel
   eau -=
@@ -69,7 +73,7 @@ for (month = 1; month < periode; month++) {
   console.log("Carottes disponibles : " + carottes + " kg");
 
 
-  verifCarottes(aMales,aFemelles,pMales,pFemelles,mCages,fCages);
+ // verifCarottes(aMales,aFemelles,pMales,pFemelles);
 
 
 
@@ -130,8 +134,13 @@ for (month = 1; month < periode; month++) {
     mAchatCaisse = readlineSync.question(
       "Combien voulez-vous acheter de caisses pour les males ? "
     );
-  } while (mAchatCaisse * prixCaisse > caisse);
+  } while ((mAchatCaisse * prixCaisse > caisse) || (mAchatCaisse === ""));
   caisse -= mAchatCaisse * prixCaisse;
+  mCages += mAchatCaisse * 1; // multiplier par 1 force l'integer, sinon passe en concatenation
+  // debug
+  console.log("machatCaisse = "+mAchatCaisse);
+  console.log("mCages = "+mCages);
+  // f debug
 
   console.log("Le prix d'un casier est de " + prixCaisse.toFixed(2) + " €");
   console.log("L'argent disponible en caisse est de " + caisse.toFixed(2) + " €");
@@ -139,8 +148,9 @@ for (month = 1; month < periode; month++) {
     fAchatCaisse = readlineSync.question(
       "Combien voulez-vous acheter de caisses pour les femelles ? "
     );
-  } while (fAchatCaisse * prixCaisse > caisse);
+  } while ((fAchatCaisse * prixCaisse > caisse) || (fAchatCaisse === ""));
   caisse -= fAchatCaisse * prixCaisse;
+  fCages += fAchatCaisse * 1; // multiplier par 1 force l'integer, sinon passe en concatenation
 
   console.log("Le prix d'un kilo de carottes est de " + prixCarottes.toFixed(2) + " €");
   console.log("L'argent disponible en caisse est de " + caisse.toFixed(2) + " €");
@@ -148,8 +158,9 @@ for (month = 1; month < periode; month++) {
     achatCarottes = readlineSync.question(
       "Combien voulez-vous acheter de kilos de carottes ? "
     );
-  } while (achatCarottes * prixCarottes > caisse);
+  } while ((achatCarottes * prixCarottes > caisse) || (achatCarottes === ""));
   caisse -= achatCarottes * prixCarottes;
+  carottes += achatCarottes * 1;
 
   console.log("Le prix du litre d'eau est de " + prixLitre.toFixed(2) + " €");
   console.log("L'argent disponible en caisse est de " + caisse.toFixed(2) + " €");
@@ -157,13 +168,14 @@ for (month = 1; month < periode; month++) {
     achatLitres = readlineSync.question(
       "Combien voulez-vous acheter de litres d'eau ? "
     );
-  } while (achatLitres * prixLitre > caisse);
+  } while ((achatLitres * prixLitre > caisse) || (achatLitres === ""));
   caisse -= achatLitres * prixLitre;
+  eau += achatLitres * 1;
 
   // portee minimale => 2 / maximale => 10
   portee = Math.round(accoup * getRandom(2, 10));
 
-  //console.log("Nb petits : " + portee);
+
 
   // repartition pm/pf
   pFemelles = Math.round(portee / 2);
