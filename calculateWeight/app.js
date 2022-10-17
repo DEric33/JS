5fc7ddb856996b14df736f70b76a0d7bcf33c992
DEric33 <eric.darmusie@darmuweb.com>
2022-10-17 12:07:32 +0200

    calculateightCorrect

--- calculateWeight/app.js
+++ calculateWeight/app.js
@@ -1,28 +1,35 @@
 // Write your function here:
 
 function calculateWeight(earthWeight, planet) {
-  switch (planet) {
-    case 'Mercure':
-      return earthWeight * 0.378;
-      break;
-    case 'Venus':
-      return earthWeight * 0.907;
-      break;
-    case 'Mars':
-      return earthWeight * 0.377;
-      break;
-    case 'Jupiter':
-      return earthWeight * 2.36;
-      break;
-    case 'Saturne':
-      return earthWeight * 0.916;
-      break;
-    default:
-      console.log("ERREUR !");
+  if (planet == "") {
+    return console.log(" Invalid Planet Entry.");
+  } else {
+    switch (planet) {
+      case "Mercure":
+        return earthWeight * 0.378;
+        break;
+      case "Venus":
+        return earthWeight * 0.907;
+        break;
+      case "Mars":
+        return earthWeight * 0.377;
+        break;
+      case "Jupiter":
+        return earthWeight * 2.36;
+        break;
+      case "Saturne":
+        return earthWeight * 0.916;
+        break;
+      default:
+        console.log(
+          "Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn."
+        );
+    }
   }
 }
 
 // Uncomment the line below when you're ready to try out your function
-console.log(calculateWeight(100, 'Jupiter')) // Should print 236
+//console.log(calculateWeight(100, 'Jupiter2')) // Should print 236
+console.log(calculateWeight(100, 0)); // Should print 236
 
 // We encourage you to add more function calls of your own to test your code!
