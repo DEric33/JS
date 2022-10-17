7348ece6c870d42a4961989f68024a7be5ad59de
DEric33 <eric.darmusie@darmuweb.com>
2022-10-17 15:08:19 +0200

    ageInYear

---
+++ calculateWeight/appsAge.js
@@ -0,0 +1,16 @@
+function howOld(age, year) {
+  let theCurrentYear = 2022;
+  const yearDifference = year - theCurrentYear;
+  const newAge = age + yearDifference;
+  if (newAge < 0) {
+    return `The year ${year} was ${Math.abs(yearDifference)} years before you were born`;
+  } else if (newAge > age) {
+    return `You will be ${newAge} in the year ${year}`;
+  } else {
+    return `You were ${newAge} in the year ${year}`;
+  }
+}
+
+//console.log(howOld(27,2030));
+console.log(howOld(39, 1957));
+//console.log(howOld(46,2022));
